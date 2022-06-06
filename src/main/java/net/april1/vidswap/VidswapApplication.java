d36d17bfd14b6be30588201303afe34627373652
prof401 <gerald.scheetz@april1.net>
2022-06-05 19:06:33 -0600

    use Mongo Atlas

--- src/main/java/net/april1/vidswap/VidswapApplication.java
+++ src/main/java/net/april1/vidswap/VidswapApplication.java
@@ -1,35 +1,30 @@
 package net.april1.vidswap;
 
 import com.mongodb.reactivestreams.client.MongoClient;
-import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
+import java.util.Arrays;
+
 @EnableReactiveMongoRepositories
 @SpringBootApplication
-public class VidswapApplication extends AbstractReactiveMongoConfiguration {
+public class VidswapApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VidswapApplication.class, args);
     }
-
+    
     @Bean
-    public MongoClient mongoClient() {
-        return MongoClients.create();
-    }
-
-    @Override
-    protected String getDatabaseName() {
-        return "games";
-    }
-
-    @Override
-    protected void configureConverters(MongoCustomConversions.MongoConverterConfigurationAdapter adapter) {
-        adapter.registerConverter(new net.april1.vidswap.converter.EventReadConverter());
+    public MongoCustomConversions mongoCustomConversions() {
+        return new MongoCustomConversions(
+                Arrays.asList(
+                        new net.april1.vidswap.converter.EventReadConverter()));
     }
 
 }
